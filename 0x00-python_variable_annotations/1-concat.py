0db5a2fcf32c2512e164c14736a12e56f2310396
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-14 20:02:22 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Definition of a function that concatenates two arrays
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1 (string): the first string
+        str2 (string): the second string
+
+    Returns:
+        string: the concatenated string
+    """
+
+    return str1 + str2
