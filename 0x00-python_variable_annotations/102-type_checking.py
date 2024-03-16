9fec61743d712cb26119d25475794e0272d10abe
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-16 14:40:27 +0100

    fix: Reformat list comprehension

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,10 +5,9 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    zoomed_in: List = [
-        item for item in lst
-        for i in range(factor)
-    ]
+    """Zoom array function
+    """
+    zoomed_in: List = [item for item in lst for i in range(factor)]
     return zoomed_in
 
 
