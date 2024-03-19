faf99c1f9d1f8997519fd95bed74001f3c41f79c
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-19 14:03:16 +0100

    fix: correct return value annotations

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Definition of async_comprehension coroutine
 """
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """Coroutine that collects 10 random numbers using an async comprehensing
     over async_generator, the return 10 random numbers
     """
