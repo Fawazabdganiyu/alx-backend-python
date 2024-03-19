da93be19a7cd2e7b09eb23788e6a361f327b1816
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-19 04:32:23 +0100

    fix: annotate function's returns properly

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """Concurrent coroutines
 """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run wait_random n times and return a list of delays in ascending order
     """
     result = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
